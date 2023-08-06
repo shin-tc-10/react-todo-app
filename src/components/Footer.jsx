38f7b8075d5bf92303011bc417ff799d64abe812
shin-tc-10 <shin.nkt.09@gmail.com>
2023-08-06 18:15:02 +0900

    各コンポーネント作成

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>©︎2023 React Todoアプリ</footer>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <main>
+        <h1>今日やること</h1>
+        <div>
+          <input type="text"></input>
+          <button>追加</button>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Main;
